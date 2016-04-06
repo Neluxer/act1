4117b06f286f72f8064279a06b181b227c3bfb31
Nelu <Nelu>
2016-04-06 20:07:54 +0100

    commit_4

---
+++ src/dam_ed04_actividad/Empleado.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dam_ed04_actividad;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Empleado {
+    private String nombre;
+    private String apellido;
+    private String dni;
+}
